22e46514dac95ef9c182b15cd293e1fffdcb787f
Bogdan F <fbogdan21@gmail.com>
2024-11-18 00:53:42 +0200

    Add files via upload

---
+++ boabe_de_orez.py
@@ -0,0 +1,8 @@
+# Legenda calatorului si a regelui
+# easy to show how the power of 2 and the power of 3 works
+
+a = int(input("Introduceti la ce putere: "))
+for i in range(a):
+	print(i,'',2**i,'',3**i)
+
+# end program
